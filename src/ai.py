61ecf577182caddb81f805d21337ccfa5e36c56c
tomg404 <32769321+tomg404@users.noreply.github.com>
2019-06-02 22:21:41 +0200

    small change to ai

--- src/ai.py
+++ src/ai.py
@@ -1,14 +1,22 @@
 import numpy as np
 import random
-class Ai:
 
+class Ai:
     def __init__(self, field, last_input, turns):
         self.field = field
         self.last_input = last_input
         self.turns = 0
+        self.player1 = "X"
+        self.player2 = "O"
+        self.empty = " "
 
     def calculate(self):
-        return random.randint(0, 7)
+        if self.last_input >= 6:
+            return self.last_input
+        elif self.last_input <= 0:
+            return self.last_input
+        else:
+            return self.last_input + 1
 
     def __check_for_first_turn(self):
         pass
@@ -17,4 +25,9 @@ class Ai:
         pass
 
     def __check_for_3_in_a_row(self):
-        pass
+        for i in range(0, 6):
+            win_row = str(self.field[i]).replace(",","").replace("'","").replace("[","").replace("]","").replace(" ", ".")
+
+            if (2*(self.player1 + ".")+self.player1) in win_row:
+                print("detected 3 in a row")
+                return True
